93556ae069bbf813ec73bc9b8a24ed38c871fd7c
ayushnvs <ayush.mishra.ald@gmail.com>
2024-01-09 22:34:42 +0530

    Updated files

--- My Plugin/HelloWorld.cs
+++ My Plugin/HelloWorld.cs
@@ -1,51 +1,53 @@
 ﻿using System.ServiceModel;
 using Microsoft.Xrm.Sdk;
 
-namespace My_Plugin;
-public class HelloWorld : IPlugin
+namespace My_Plugin
 {
-    public void Execute(IServiceProvider serviceProvider)
+    public class HelloWorld : IPlugin
     {
-        // Obtain the tracing service
-        ITracingService? tracingService =
-        (ITracingService?)serviceProvider.GetService(typeof(ITracingService));
-
-        // Obtain the execution context from the service provider.  
-        IPluginExecutionContext? context = (IPluginExecutionContext?)
-            serviceProvider.GetService(typeof(IPluginExecutionContext));
-
-        // The InputParameters collection contains all the data passed in the message request.  
-        if (context.InputParameters.Contains("Target") &&
-            context.InputParameters["Target"] is Entity)
+        public void Execute(IServiceProvider serviceProvider)
         {
-            // Obtain the target entity from the input parameters.  
-            Entity entity = (Entity)context.InputParameters["Target"];
+            // Obtain the tracing service
+            ITracingService? tracingService =
+            (ITracingService?)serviceProvider.GetService(typeof(ITracingService));
 
-            // Obtain the IOrganizationService instance which you will need for  
-            // web service calls.  
-            IOrganizationServiceFactory? serviceFactory =
-                (IOrganizationServiceFactory?)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-            IOrganizationService? service = serviceFactory?.CreateOrganizationService(context.UserId);
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext? context = (IPluginExecutionContext?)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
 
-            try
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
             {
-                // Reading from attribute values
-                string? firstName = entity.Attributes["firstname"].ToString();
-                string? lastName = entity.Attributes["lastname"].ToString();
+                // Obtain the target entity from the input parameters.  
+                Entity entity = (Entity)context.InputParameters["Target"];
 
-                // Assign data to description attribute
-                entity.Attributes.Add("description", "Hello World" + firstName + lastName);
-            }
+                // Obtain the IOrganizationService instance which you will need for  
+                // web service calls.  
+                IOrganizationServiceFactory? serviceFactory =
+                    (IOrganizationServiceFactory?)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService? service = serviceFactory?.CreateOrganizationService(context.UserId);
 
-            catch (FaultException<OrganizationServiceFault> ex)
-            {
-                throw new InvalidPluginExecutionException("An error occurred in FollowUpPlugin.", ex);
-            }
+                try
+                {
+                    // Reading from attribute values
+                    string? firstName = entity.Attributes["firstname"].ToString();
+                    string? lastName = entity.Attributes["lastname"].ToString();
 
-            catch (Exception ex)
-            {
-                tracingService?.Trace("FollowUpPlugin: {0}", ex.ToString());
-                throw;
+                    // Assign data to description attribute
+                    entity.Attributes.Add("description", "Hello World" + firstName + lastName);
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in FollowUpPlugin.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService?.Trace("FollowUpPlugin: {0}", ex.ToString());
+                    throw;
+                }
             }
         }
     }
