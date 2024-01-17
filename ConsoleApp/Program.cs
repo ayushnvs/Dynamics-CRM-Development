1acceffb0b6fc91abb2056d0014b150eb8d9fda9
ayushnvs <ayush.mishra.ald@gmail.com>
2024-01-17 09:57:31 +0530

    console app update

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
 
 namespace ConsoleApp
 {
@@ -10,6 +12,20 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            // Authenticate
+            string connectionString = "AuthType=Office365;Url=https://orgeb294ea2.crm8.dynamics.com;Username=admin@dynamicscrm504.onmicrosoft.com;Password=Hsdfj566@djf";
+
+            CrmServiceClient service = new CrmServiceClient(connectionString);
+
+            // Create a contact in dynamics 365
+
+            Entity contact = new Entity("contact");
+            contact.Attributes.Add("lastname", "consoleapp");
+
+            Guid guid = service.Create(contact);
+
+            Console.WriteLine(guid);
+            Console.Read();
         }
     }
 }
